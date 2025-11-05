5b618593763aab95b0f568d87c00bef4b2628974
KaraParikh1 <karaaparikh@gmail.com>
2025-11-05 12:22:27 -0500

    did first step of outline

--- chapter7excersize0.py
+++ chapter7excersize0.py
@@ -1,5 +1,10 @@
-#add second version of 'find' function
-
+def find(str, ch, start=0):
+    index = start
+    while index < len(str):
+        if str[index] == ch:
+            return index
+        index = index + 1
+    return -1
 
 #add parameter in parameter list
 
