12aef1eb174f7f41feb98f55c134450c7acaa945
KaraParikh1 <karaaparikh@gmail.com>
2025-11-05 12:25:47 -0500

    deleted instruction comment we forgot to delete on the last commit

--- chapter7excersize0.py
+++ chapter7excersize0.py
@@ -6,7 +6,6 @@ def find(str, ch, start=0, end=0):
         index = index + 1
     return -1
 
-#add parameter in parameter list
 
 
 #at beginning of function, use len() of string to add value to end parameter
